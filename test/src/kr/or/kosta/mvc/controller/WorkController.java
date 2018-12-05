05a4ff4eda5b46f5b329719e448d0e330cea748c
KOSTA <KOSTA@KOSTA-PC>
2018-12-05 10:09:35 +0900

    retest

---
+++ test/src/kr/or/kosta/mvc/controller/WorkController.java
@@ -0,0 +1,12 @@
+package kr.or.kosta.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class WorkController {
+
+	@RequestMapping("/val")
+	public String test() {
+		return "hitest";
+	}
+}
