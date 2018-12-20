d3a485c0e2972e892359fd11579378ef7368f0a9
KOSTA <KOSTA@KOSTA-PC>
2018-12-20 11:02:16 +0900

    kokokokoko

---
+++ spring1105/src/ex1/MyNameBean.java
@@ -0,0 +1,13 @@
+package ex1;
+
+public class MyNameBean {
+	private String personname;
+
+	public void setPersonname(String personname) {
+		this.personname = personname;
+	}
+	
+	public String printName() {
+		return personname;
+	}
+}
